053ed96e264301d22fc015523ff5821c26bff2b8
wsman217 <44420280+wsman217@users.noreply.github.com>
2023-10-01 15:25:18 -0500

    Update javascript.js
    WHY WONT YOU LET ME COMMIT

--- javascript.js
+++ javascript.js
@@ -1,69 +1,98 @@
-var gameWindow = document.querySelector("grid-container");
-
-// Arrays for setting up rows and columns.
-const rows = new Array(0, 1, 2)
-const cols = new Array(0, 1, 2)
-const winningMoves = new Array(
-    [0, 1, 2], // Horizontal, Row 0
-    [3, 4, 5], // Horizontal, Row 1
-    [6, 7, 8], // Horizontal, Row 2
-    [0, 3, 6], // Vertical, Column 0
-    [1, 4, 7], // Vertical, Column 1
-    [2, 5, 8], // Vertical, Column 2
-    [0, 4, 8],  // Diagonal, Left to Right
-    [2, 4, 6]  // Diagonal, Right to Left
-)
+const winningMoves = [
+    [
+        true, true, true,
+        false, false, false,
+        false, false, false
+    ], // Horizontal, Row 0
+    [
+        false, false, false,
+        true, true, true,
+        false, false, false
+    ], // Horizontal, Row 1
+    [
+        false, false, false,
+        false, false, false,
+        true, true, true
+    ], // Horizontal, Row 2
+    [
+        true, false, false,
+        true, false, false,
+        true, false, false
+    ], // Vertical, Column 0
+    [
+        false, true, false,
+        false, true, false,
+        false, true, false
+    ], // Vertical, Column 1
+    [
+        false, false, true,
+        false, false, true,
+        false, false, true
+    ], // Vertical, Column 2
+    [
+        true, false, false,
+        false, true, false,
+        false, false, true
+    ],  // Diagonal, Left to Right
+    [
+        false, false, true,
+        false, true, false,
+        true, false, false
+    ] // Diagonal, Right to Left
+]
+let currentBoard = new Array(9).fill(0)
+let currentPlayer = false
 
 // Function for registering button presses.
-function buttonPressed(id){
-    switch(id){
-        case "button0":
-            alert(id)
-            break
-        case "button1":
-            alert(id)
-            break
-        case "button2":
-            alert(id)
-            break
-        case "button3":
-            alert(id)
-            break
-        case "button4":
-            alert(id)
-            break
-        case "button5":
-            alert(id)
-            break
-        case "button6":
-            alert(id)
-            break
-        case "button7":
-            alert(id)
-            break
-        case "button8":
-            alert(id)
-            break
-        case "buttonStart":
-            alert(id)
-            gameLoop()
-            break
-        case "buttonClear":
-            alert(id)
-            clearBoard()
-            break
-        default:
-            alert("Default case")
-            break
+function buttonPressed(id) {
+    if (currentBoard[id] !== 0) {
+        console.log("Already played implement something here lol.")
+        return
     }
+
+    currentBoard[id] = currentPlayer
+    updateBoard()
+
+    if (checkBoard()) {
+        // win case
+        console.log("won")
+        return
+    }
+
+    currentPlayer = !currentPlayer
 }
 
-function gameLoop(){
-    while(True){
-        
+function checkBoard() {
+    let playerState = new Array(currentBoard.length).fill(false)
+
+    currentBoard.forEach((value, index) => {
+        if (value === currentPlayer) {
+            playerState[index] = true
+        }
+    })
+
+    for (let value in winningMoves) {
+        if (winningMoves[value].toString() === playerState.toString())
+            return true
     }
+
+    return false
 }
 
-function clearBoard(){
+function updateBoard() {
+    currentBoard.forEach((value, index) => {
+        let currentMarker = ""
 
+        if (value === true) {
+            currentMarker = "X"
+        } else if (value === false) {
+            currentMarker = "O"
+        }
+        let currentIndexButton = document.getElementById(index.toString())
+        currentIndexButton.innerText = currentMarker
+    })
+}
+
+function clearBoard() {
+    currentBoard = new Array(9).fill(0)
 }
