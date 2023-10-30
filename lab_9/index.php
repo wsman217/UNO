18f9641e4234d3635f5383b81a093584abe7efa6
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-10-30 15:23:43 -0500

    Final lab

---
+++ lab_9/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>COSC 4351 - Lab 9</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="body">
+<header class="header">
+    <h1>Search by the top 50 songs on Spotify</h1>
+</header>
+<form method="get">
+    <label>
+        <input type="text" name="to_search">
+    </label>
+    <!--input type="button" value="click"-->
+    <button type='button'>submit</button>
+</form>
+<?php
+$topSongs = fopen("./universal_top_spotify_songs.csv", "r");
+if ($topSongs && isset($_GET["to_search"])) {
+    echo "<table>";
+    foreach (explode(",", fgets($topSongs)) as $title) {
+        echo "<th>" . $title . "</th>";
+    }
+    while ($currentLine = fgets($topSongs)) {
+        if (str_contains(strtolower($currentLine), strtolower($_GET["to_search"]))) {
+            echo "<tr>";
+            foreach (explode(",", $currentLine) as $element) {
+                echo "<td>" . $element . "</td>";
+            }
+        }
+    }
+    echo "</table>";
+}
+?>
+</body>
+<footer>
+    <p>Designed and built by <i>Christian Wade</i>, <i>Weston Sublett</i>, and <i>Hayden Rawlings</i></p>
+</footer>
+</html>
