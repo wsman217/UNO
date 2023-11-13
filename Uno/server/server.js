355d3fa8e64fef6ca89925606b1ef40e5dc2f1d1
weston <westonsublett1@gmail.com>
2023-11-13 17:38:07 -0600

    Basic usage of Socket io

--- Uno/server/server.js
+++ Uno/server/server.js
@@ -1,2 +1,9 @@
-const { Server } = require("socket.io");
+http = require('http').createServer().listen(3000, "localhost");
+const {Server} = require("socket.io");
+const io = new Server(http);
 
+io.on('connection', (socket) => {
+    socket.on("yeet", () => {
+        console.log("yeet");
+    })
+})
