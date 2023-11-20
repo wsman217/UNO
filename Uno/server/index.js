1bd325e7e89e43bb7aec99692255c8cfa19d24ab
weston <westonsublett1@gmail.com>
2023-11-20 15:19:19 -0600

    Working on the server side.

--- Uno/server/index.js
+++ Uno/server/index.js
@@ -3,19 +3,59 @@ http = require('http').createServer().listen(process.env.SERVER_PORT, process.en
 const {Server} = require("socket.io");
 const io = new Server(http);
 
-const currentPlayers = new Map();
+const currentPlayers = [];
+const servers = new Map();
 
-console.log(process.env.SERVER_PORT)
+/**
+ * Response codes are loosely based on html
+Response codes:
+ 200: ok
+ 418: name exists already
+ 404: not found
+ */
 
 io.on('connection', (socket) => {
     socket.on("setUsername", (username, ackFunction) => {
-        console.log("username set");
-
-        if (currentPlayers.has(username)) {
+        if (currentPlayers.includes(username)) {
             ackFunction(418)
         }
 
-        currentPlayers.set(username, socket)
+        currentPlayers.push(username)
+        ackFunction(200)
+    })
+
+    socket.on("createServer", (username, serverName, ackFunction) => {
+        if (servers.has(serverName)) {
+            ackFunction(418)
+        }
+
+        let gameServer = new GameServer(serverName, username)
+        servers.set(serverName, gameServer)
+        ackFunction(200)
+    })
+
+    socket.on("joinServer", (username, serverName, ackFunction) => {
+        if (!servers.has(serverName)) {
+            ackFunction(404)
+        }
+
+        let gameServer = servers.get(serverName)
+        gameServer.addPlayer(username, socket);
         ackFunction(200)
     })
 })
+
+// TODO break this out into a separate file once we figure out how to do that lol
+class GameServer {
+    constructor(serverName, host) {
+        this.serverName = serverName
+        this.host = host
+        this.players = new Map()
+        // Array of tuples
+        this.moves = []
+    }
+
+    addPlayer(username, socket) {
+        this.players.set(username, socket)
+    }
+}
