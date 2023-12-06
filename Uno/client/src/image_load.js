1daabc6ef9e237db65205872acc3cc7433f8c346
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-12-06 16:46:40 -0600

    Add map file

---
+++ Uno/client/src/image_load.js
@@ -0,0 +1,64 @@
+const mapCard = new Map();
+
+mapCard.set('Z', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/black/card_plus_4_black.png");
+mapCard.set('W', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/black/card_wild_black.png");
+
+mapCard.set('B1', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_1_blue.png");
+mapCard.set('B2', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_2_blue.png");
+mapCard.set('B3', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_3_blue.png");
+mapCard.set('B4', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_4_blue.png");
+mapCard.set('B5', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_5_blue.png");
+mapCard.set('B6', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_6_blue.png");
+mapCard.set('B7', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_7_blue.png");
+mapCard.set('B8', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_8_blue.png");
+mapCard.set('B9', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_9_blue.png");
+mapCard.set('BD', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_plus_2_blue.png");
+mapCard.set('BZ', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_plus_4_blue.png");
+mapCard.set('BR', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_reverse_blue.png");
+mapCard.set('BS', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_skip_blue.png");
+mapCard.set('BW', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/blue/card_wild_blue.png");
+
+mapCard.set('G1', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_1_green.png");
+mapCard.set('G2', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_2_green.png");
+mapCard.set('G3', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_3_green.png");
+mapCard.set('G4', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_4_green.png");
+mapCard.set('G5', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_5_green.png");
+mapCard.set('G6', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_6_green.png");
+mapCard.set('G7', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_7_green.png");
+mapCard.set('G8', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_8_green.png");
+mapCard.set('G9', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_9_green.png");
+mapCard.set('GD', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_plus_2_green.png");
+mapCard.set('GZ', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_plus_4_green.png");
+mapCard.set('GR', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_reverse_green.png");
+mapCard.set('GS', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_skip_green.png");
+mapCard.set('GW', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/green/card_wild_green.png");
+
+mapCard.set('R1', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_1_red.png");
+mapCard.set('R2', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_2_red.png");
+mapCard.set('R3', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_3_red.png");
+mapCard.set('R4', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_4_red.png");
+mapCard.set('R5', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_5_red.png");
+mapCard.set('R6', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_6_red.png");
+mapCard.set('R7', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_7_red.png");
+mapCard.set('R8', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_8_red.png");
+mapCard.set('R9', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_9_red.png");
+mapCard.set('RD', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_plus_2_red.png");
+mapCard.set('RZ', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_plus_4_red.png");
+mapCard.set('RR', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_reverse_red.png");
+mapCard.set('RS', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_skip_red.png");
+mapCard.set('RW', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/red/card_wild_red.png");
+
+mapCard.set('Y1', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_1_yellow.png");
+mapCard.set('Y2', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_2_yellow.png");
+mapCard.set('Y3', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_3_yellow.png");
+mapCard.set('Y4', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_4_yellow.png");
+mapCard.set('Y5', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_5_yellow.png");
+mapCard.set('Y6', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_6_yellow.png");
+mapCard.set('Y7', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_7_yellow.png");
+mapCard.set('Y8', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_8_yellow.png");
+mapCard.set('Y9', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_9_yellow.png");
+mapCard.set('YD', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_plus_2_yellow.png");
+mapCard.set('YZ', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_plus_4_yellow.png");
+mapCard.set('YR', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_reverse_yellow.png");
+mapCard.set('YS', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_skip_yellow.png");
+mapCard.set('YW', "https://raw.githubusercontent.com/sudge64/COSC-4351/main/Uno/images/yellow/card_wild_yellow.png");
